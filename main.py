b54553f93ff0d88066233acb16db9f18fafb4a03
IamAbler <abler623887161@outlook.com>
2021-11-20 22:59:42 +0800

    v1.2
    移除goods_name参数
    优化代码结构

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import time
 
 game = input("game:\n")
 goods_id = input("goods_id:\n")
-goods_name = input("goods_name:\n")
 SleepTime = int(input("SleepTime:\n"))
 cookie = input("cookie:\n")
 file = open("HistoryPrice.txt", 'a')
@@ -14,15 +13,23 @@ if game == "":
 
 while True:
     InfoJson = getinfo.getinfo(game, goods_id)
-    NumJson = getinfo.getnum(game, goods_name, cookie)
-
-    num = json.loads(NumJson)
     info = json.loads(InfoJson)
 
     info_data = info['data']
     info_items = info_data['items']
     info_item = info_items[0]
     info_price = info_item['price']
+    goods_infos = info_data['goods_infos']
+    goods_info = goods_infos[goods_id]
+    goods_name = goods_info['name']
+
+    NumJson = getinfo.getnum(game, goods_name, cookie)
+    num = json.loads(NumJson)
+
+    num_data = num['data']
+    num_items = num_data['items']
+    num_item = num_items[0]
+    sell_num = num_item['sell_num']
 
     num_data = num['data']
     num_items = num_data['items']
